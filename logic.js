b5d73393d5f6018ab04bbdd57b14d141e7545401
Chaucee <hi@chaucee.com>
2018-01-07 19:55:53 -0700

    logic added

--- logic.js
+++ logic.js
@@ -1,24 +1,61 @@
-/* global firebase moment */
-// Steps to complete:
-
-// 1. Initialize Firebase
-// 2. Create button for adding new train - then update the html + update the database
-// 3. Create a way to retrieve employees from the employee database.
-// 4. Create a way to calculate the months worked. Using difference between start and current time.
-//    Then use moment.js formatting to set difference in months.
-// 5. Calculate Total billed
+// Create button for adding new train - then update the html + update the database
 
 
- // Initialize Firebase
- var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyCyn0w7IfN1imwG8EAPeVlNXKamzMDbRTA",
     authDomain: "train-time-f686e.firebaseapp.com",
     databaseURL: "https://train-time-f686e.firebaseio.com",
     projectId: "train-time-f686e",
     storageBucket: "",
     messagingSenderId: "606866579867"
-  };
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
+
+database.ref().on("value", function(snapshot) {
+
+console.log(snapshot.val()); 
+
+
+// Button for adding Trains
+$("#add-train-btn").on("click", function(event) {
+event.preventDefault();
+
+// Grabs user input
+var empName = $("#train-name-input").val().trim();
+var empDestination = $("#destination-input").val().trim();
+var empFreq = moment($("#freq-input").val().trim(), "DD/MM/YY").format("X");
+var empArrival = $("#arrival-input").val().trim();
+
+// Creates local "temporary" object for holding employee data
+var newEmp = {
+name: empName,
+role: empDestination,
+start: empFreq,
+rate: empRate
+};
+
+
+// Uploads employee data to the database
+database.ref().push(newEmp);
+
+// Logs everything to console
+console.log(newEmp.name);
+console.log(newEmp.destination);
+console.log(newEmp.freq);
+console.log(newEmp.arrival);})})
+
+// Clears all of the text-boxes
+$("#train-name-input").val("");
+$("#destination-input").val("");
+$("#freq-input").val("");
+$("#arrival-input").val("");
+
+
+// Add train data into the table
+$("#train-table > tbody").append("<tr><td>" + empName + "</td><td>" + empDestination + "</td><td>" + empArrival + "</td><td>" + empFreq + "</td><td>");
+
+
