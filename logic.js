d616a16736f51afe32b686214df8bf0d7a6a0925
Chaucee <hi@chaucee.com>
2018-01-07 17:51:52 -0700

    my design

---
+++ logic.js
@@ -0,0 +1,9 @@
+/* global firebase moment */
+// Steps to complete:
+
+// 1. Initialize Firebase
+// 2. Create button for adding new employees - then update the html + update the database
+// 3. Create a way to retrieve employees from the employee database.
+// 4. Create a way to calculate the months worked. Using difference between start and current time.
+//    Then use moment.js formatting to set difference in months.
+// 5. Calculate Total billed
